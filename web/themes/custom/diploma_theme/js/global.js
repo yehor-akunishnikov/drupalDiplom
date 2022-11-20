5fc322b730be228be2cacfde59a016235c5dcbb2
Bohdan Horb <wannaalwaysbe@gmail.com>
2022-11-20 18:22:50 +0200

    Add initial theming.

---
+++ web/themes/custom/diploma_theme/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.diploma_theme = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
